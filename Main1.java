8bec7f8124479490e07fb5bac35e2a2cccd377ed
Zofia Well <zofia.wellenger@gmail.com>
2019-05-05 11:08:24 +0200

    Zadanie nr 1 z kontroli przeplywu

---
+++ Main1.java
@@ -0,0 +1,15 @@
+
+public class Main1 {
+
+    public static void main(String[] args) {
+	int nr1 = 5;
+	int nr2 = 89;
+	System.out.println("Liczba 1 to: " + nr1 + ", Liczba 2 to: " + nr2);
+	if (nr1 > nr2) {
+		System.out.println("Wieksza liczba to " +nr1);
+	} else {
+		System.out.println("Wieksza liczba to " +nr2);
+	}
+    }
+
+}
