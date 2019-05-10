54a03705a22648b76f4b6dd8e2ef0577e5091aa0
Zofia Well <zofia.wellenger@gmail.com>
2019-05-10 15:14:48 +0200

    zadanie nr 4

--- Main4.java
+++ Main4.java
@@ -8,7 +8,14 @@
 public class Main4 {
 
     public static void main(String[] args) {
-
+    for (int i=1; i <=100; i = i+1) {
+        System.out.print(i + ", ");
+    }
+    int j=1;
+    while (j<=100){
+        System.out.print(j + ", ");
+        j=j+1;
+        }
     }
 
 }
