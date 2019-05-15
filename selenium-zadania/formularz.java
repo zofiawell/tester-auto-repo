1bb9735b459e0448b6bd145c5e11d3f0e85a356b
Zofia Well <zofia.wellenger@gmail.com>
2019-05-15 14:50:26 +0200

    nowe testy w klasie formularz

--- selenium-zadania/formularz.java
+++ selenium-zadania/formularz.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class formularz {
 
@@ -15,7 +19,7 @@ public class formularz {
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","/home/zofia/tester-automat-kurs/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/home/zofia/tester-automat-kurs/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
@@ -32,14 +36,14 @@ public class formularz {
         driver.findElement(By.id("last-name")).sendKeys("Kowalski");
         List<WebElement> elements = driver.findElements(By.cssSelector(".radio-inline"));
 
-        for(int i=0; i < elements.size(); i++) {
+        for (int i = 0; i < elements.size(); i++) {
             if (elements.get(i).getText().equals("Female")) {
                 elements.get(i).click();
                 break;
             }
         }
         for (WebElement element : elements) {
-            if (element.getText().equals("Female")){
+            if (element.getText().equals("Female")) {
                 element.click();
                 break;
             }
@@ -58,11 +62,28 @@ public class formularz {
         driver.findElement(By.xpath("//label[text()= 'Join tech cons']")).click();
         driver.findElement(By.id("comment")).sendKeys("To jest mój pierwszy automat testowy");
         driver.findElement(By.id("submit")).click();
+        String message = driver.findElement(By.id("submit-msg")).getText();
+        assertEquals("Successfully submitted!", message);
+
 
         System.out.println("Koniec");
+    }
 
+    @Test
+    public void checkErrors() {
+        driver.get("https://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
+        driver.findElement(By.id("submit")).click();
+//            List<String> listOfIds = getListOfIds();
+        List<String> listOfIds = Arrays.asList("first-name", "last-name", "gender", "dob", "address", "email", "password", "company");
+        int counter = 0;
 
-
+        for (String elementLocator : listOfIds) {
+            elementLocator = elementLocator + "-error";
+            assertEquals("This field is required.", driver.findElement(By.id(elementLocator)).getText());
+            counter++;
+        }
+        System.out.println(counter);
+    }
 
 //        System.out.println(elements.get(0).getText());
 //        System.out.println(elements.get(1).getText());
@@ -70,5 +91,35 @@ public class formularz {
 //
 //        elements.get(0).click();
 
+
+    private List<String> getListOfIds() {
+        List<String> listID = new ArrayList<>();
+
+
+        return listID;
+    }
+
+
+    @Test
+    public void checkEmail() {
+        driver.get("https://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
+        driver.findElement(By.id("email")).sendKeys("w");
+        driver.findElement(By.id("submit")).click();
+        String message = driver.findElement(By.id("email-error")).getText();
+        assertEquals("Please enter a valid email address.", message);
+
+    }
+
+    @Test
+    public void checkName() {
+        driver.get("https://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
+        driver.findElement(By.id("first-name")).sendKeys("w");
+        driver.findElement(By.id("submit")).click();
+        String messageFirst = driver.findElement(By.id("first-name-error")).getText();
+        assertEquals("Please enter a valid first name.", messageFirst);
+        driver.findElement(By.id("last-name")).sendKeys("w");
+        driver.findElement(By.id("submit")).click();
+        String messageLast = driver.findElement(By.id("last-name-error")).getText();
+        assertEquals("Please enter a valid last name.", messageLast);
     }
 }
